87c8b8a18a8fda545a39f2be950a9904ce92d479
zipishuseyinli25 <123469457+zipishuseyinli25@users.noreply.github.com>
2023-11-08 03:40:30 +0400

    Azccriminal.py

---
+++ azccriminal.py
@@ -0,0 +1,18 @@
+from criminal import azccriminal
+
+azccriminal = "All-urls"
+criminalshot = "huntershooting, webshooting, criminalshooting, sslshooting, bruteforceshooting, web app"
+shootingmode = "Tüm veritabanı ve site iletişim araçları ve genel kurulum bilgilerini yakalama"
+azccriminalshooting = "Tüm sızma entegrasyonu"
+shootcriminal = azccriminal
+criminalaz = "dil entegrasyonu"
+azccriminaldatabase = "criminalshoot and criminal"
+from setuptools import setup, find_packages
+
+setup(
+    name="azccriminal",
+    version="1.0.0",
+    packages=find_packages(),
+)
+[build-system]
+requires = ["azccriminaltools>=42", "wheel"]
